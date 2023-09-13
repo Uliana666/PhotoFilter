492d5ce4af18ed5425e14b73510af2b903786869
Uliana Parkina <uliana.parkina@gmail.com>
2023-09-13 19:53:09 +0300

    palindrome if empty line

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -4,8 +4,11 @@ bool IsPalindrome(const std::string& str) {
     size_t l = 0;
     size_t r = str.size() - 1;
     while (r > l) {
-        while (str[l] == ' ') {
+        while (str[l] == ' ' && l < str.size()) {
             l++;
+        }
+        if (l == str.size()) {
+            return true;
         }
         while (str[r] == ' ') {
             r--;
