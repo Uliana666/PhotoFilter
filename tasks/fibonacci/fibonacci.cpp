e8f813ecdca6ff0e769c5c47d4c4e99881075ac2
Uliana Parkina <uliana.parkina@gmail.com>
2023-09-13 18:50:49 +0300

    Solve fibonacci

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -1,5 +1,14 @@
 #include "fibonacci.h"
+#include <vector>
 
 int64_t CalculateNthFibonacci(int64_t n) {
-    return 0;
+    if (n <= 1) {
+        return n;
+    }
+    std::vector<int64_t> dp(n + 1);
+    dp[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
 }
