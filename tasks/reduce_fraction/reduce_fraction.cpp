e7e908c23d764d117db01de28efcf8b8988cb181
Uliana Parkina <uliana.parkina@gmail.com>
2023-09-13 19:07:44 +0300

    Solve reduce fraction

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -1,6 +1,11 @@
 #include "reduce_fraction.h"
-
+#include <algorithm>
 
 int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
-    return 1;
+    int64_t num_copy = numerator;
+    int64_t den_copy = denominator;
+    while (den_copy) {
+        num_copy %= den_copy, std::swap(num_copy, den_copy);
+    }
+    return (numerator + denominator) / num_copy;
 }
