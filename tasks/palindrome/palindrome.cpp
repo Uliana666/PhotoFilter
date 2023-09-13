64e4907b8ec18d45e3283a912933aa194de4911b
Uliana Parkina <uliana.parkina@gmail.com>
2023-09-13 19:15:32 +0300

    Solve palindrome

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,5 +1,20 @@
 #include "palindrome.h"
 
 bool IsPalindrome(const std::string& str) {
-    return false;
+    size_t l = 0;
+    size_t r = str.size() - 1;
+    while (r > l) {
+        while (str[l] == ' ') {
+            l++;
+        }
+        while (str[r] == ' ') {
+            r--;
+        }
+        if (str[r] != str[l]) {
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
 }
