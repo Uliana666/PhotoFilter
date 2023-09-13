517775eb98288b1e6f411404d39fc22d1ff1d8fa
Uliana Parkina <uliana.parkina@gmail.com>
2023-09-13 19:47:14 +0300

    Solve task password

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -1,5 +1,40 @@
 #include "password.h"
+#include <cctype>
+
+struct LEN {
+    enum : int { MIN = 8, MAX = 14 };
+};
+
+struct CODE {
+    enum : int { MIN = 33, MAX = 126 };
+};
+
+struct TYPES {
+    enum : int { BIG_LET = 0, SMALL_LET, DIGIT, OTHER, NUMBER_OF_TYPES };
+};
 
 bool ValidatePassword(const std::string& password) {
-    return false;
+    bool used[TYPES::NUMBER_OF_TYPES + 1];
+    if (password.size() < LEN::MIN || password.size() > LEN::MAX) {
+        return false;
+    }
+    for (size_t i = 0; i < password.size(); ++i) {
+        if (password[i] < CODE::MIN || password[i] > CODE::MAX) {
+            return false;
+        }
+        if (isupper(password[i])) {
+            used[TYPES::BIG_LET] = true;
+        } else if (islower(password[i])) {
+            used[TYPES::SMALL_LET] = true;
+        } else if (isdigit(password[i])) {
+            used[TYPES::DIGIT] = true;
+        } else {
+            used[TYPES::OTHER] = true;
+        }
+    }
+    size_t count = 0;
+    for (size_t i = 0; i < TYPES::NUMBER_OF_TYPES; ++i) {
+        count += used[i];
+    }
+    return count >= 3;
 }
