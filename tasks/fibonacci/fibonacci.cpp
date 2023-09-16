ac3052f6298616b24273e6b2adfeaf0bdd2d5e4e
Uliana Parkina <uliana.parkina@gmail.com>
2023-09-16 18:30:20 +0300

    delete dp

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -5,10 +5,12 @@ int64_t CalculateNthFibonacci(int64_t n) {
     if (n <= 1) {
         return n;
     }
-    std::vector<int64_t> dp(n + 1);
-    dp[1] = 1;
-    for (int i = 2; i <= n; ++i) {
-        dp[i] = dp[i - 1] + dp[i - 2];
+    int64_t fibonacci0 = 0;
+    int64_t fibonacci1 = 1;
+    for (size_t i = 2; i <= n; ++i) {
+        int64_t fibonacci2 = fibonacci0 + fibonacci1;
+        fibonacci0 = fibonacci1;
+        fibonacci1 = fibonacci2;
     }
-    return dp[n];
+    return fibonacci1;
 }
