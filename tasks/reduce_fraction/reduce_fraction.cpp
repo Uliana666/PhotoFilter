82483a305f0f21dba317bfbdecc9abd528ff5143
Uliana Parkina <uliana.parkina@gmail.com>
2023-09-16 18:24:14 +0300

    use std::gcd

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -1,11 +1,7 @@
 #include "reduce_fraction.h"
-#include <algorithm>
+
+#include <numeric>
 
 int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
-    int64_t num_copy = numerator;
-    int64_t den_copy = denominator;
-    while (den_copy) {
-        num_copy %= den_copy, std::swap(num_copy, den_copy);
-    }
-    return (numerator + denominator) / num_copy;
+    return (numerator + denominator) / std::gcd(numerator, denominator);
 }
