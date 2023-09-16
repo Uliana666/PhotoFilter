010ce1712947af579f3b154961b5adb722854824
Uliana Parkina <uliana.parkina@gmail.com>
2023-09-16 18:35:07 +0300

    delete if

--- tasks/chocolate/chocolate.cpp
+++ tasks/chocolate/chocolate.cpp
@@ -1,10 +1,6 @@
 #include "chocolate.h"
 
 bool CanBreakSlices(int height, int width, int slices_amount) {
-    if (slices_amount % height == 0 && slices_amount / height < width) {
-        return true;
-    } else if (slices_amount % width == 0 && slices_amount / width < height) {
-        return true;
-    }
-    return false;
+    return ((slices_amount % height == 0 && slices_amount / height < width) ||
+            (slices_amount % width == 0 && slices_amount / width < height));
 };
