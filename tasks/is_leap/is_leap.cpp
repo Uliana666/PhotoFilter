faee5f2f1bda8ffcd9ce03e247e6c3cad60b8127
Uliana Parkina <uliana.parkina@gmail.com>
2023-09-16 18:20:03 +0300

    define -> const

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -1,12 +1,13 @@
 #include "is_leap.h"
 
-#define YEAR_CONST1 4
-#define YEAR_CONST2 100
+const int YEAR_CONST1 = 4;
+const int YEAR_CONST2 = 100;
+const int CONST1_CONST2 = 400;
 
 bool IsLeap(int year) {
     if (year % YEAR_CONST1 == 0 && year % YEAR_CONST2 != 0) {
         return true;
     } else {
-        return year % (YEAR_CONST1 * YEAR_CONST2) == 0;
+        return year % (CONST1_CONST2) == 0;
     }
 }
