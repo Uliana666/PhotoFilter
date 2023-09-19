f9b38e8f27bc267df41202e3379471000a4d93f8
Uliana Parkina <uliana.parkina@gmail.com>
2023-09-19 18:37:59 +0300

    size_t....

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -1,14 +1,12 @@
 #include "fibonacci.h"
 
-#include <cstddef>
-
 int64_t CalculateNthFibonacci(int64_t n) {
     if (n <= 1) {
         return n;
     }
     int64_t fibonacci0 = 0;
     int64_t fibonacci1 = 1;
-    for (std::size_t i = 2; i <= n; ++i) {
+    for (int64_t i = 2; i <= n; ++i) {
         int64_t fibonacci2 = fibonacci0 + fibonacci1;
         fibonacci0 = fibonacci1;
         fibonacci1 = fibonacci2;
