ecc3a3f21c8ce21ba8328e8c0394406022292ca3
Uliana Parkina <uliana.parkina@gmail.com>
2023-09-27 16:48:48 +0300

    foxed extra changes

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -1,16 +1,6 @@
 #include "tests_checking.h"
 
-#include <deque>
-
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    std::deque<std::string> order_of_notebooks;
-    for (auto action : student_actions) {
-        if (action.side == Side::Top) {
-            order_of_notebooks.push_front(action.name);
-        } else {
-            order_of_notebooks.push_back(action.name);
-        }
-    }
     return {};
 }
