70c8d425c62808e370e7424068fa504cadef1945
Uliana Parkina <uliana.parkina@gmail.com>
2023-09-27 17:52:21 +0300

    solve

--- tasks/admission/admission.cpp
+++ tasks/admission/admission.cpp
@@ -14,16 +14,35 @@ bool Date::operator!=(const Date& other) const {
     return this->operator<(other) || other < *this;
 }
 
-bool CompareApplicant(const Applicant& a, const Applicant& b){
-    if (a.points != b.points){
-        return a.points > b.points;
+bool CompareApplicant(const Applicant* a, const Applicant* b) {
+    if (a->points != b->points) {
+        return a->points > b->points;
     }
-    if (a.student.birth_date != b.student.birth_date){
-        return a.student.birth_date < b.student.birth_date;
+    if (a->student.birth_date != b->student.birth_date) {
+        return a->student.birth_date < b->student.birth_date;
     }
+    return a->student.name < b->student.name;
 }
 
 AdmissionTable FillUniversities(const std::vector<University>& universities, const std::vector<Applicant>& applicants) {
-    std::vector<Applicant> copy_applicant = applicants;
-    sort(copy_applicant.begin(), copy_applicant.end(), []())
+    std::vector<const Applicant*> copy_applicants;
+    for (const auto& applicant : applicants) {
+        copy_applicants.push_back(&applicant);
+    }
+    sort(copy_applicants.begin(), copy_applicants.end(), CompareApplicant);
+    AdmissionTable result;
+    std::unordered_map<std::string, size_t> number_of_places;
+    for (const auto& university : universities) {
+        number_of_places[university.name] = university.max_students;
+    }
+    for (const auto& applicant : copy_applicants) {
+        for (const auto& name_of_university : applicant->wish_list) {
+            if (number_of_places[name_of_university] > 0) {
+                --number_of_places[name_of_university];
+                result[name_of_university].push_back(&applicant->student);
+                break;
+            }
+        }
+    }
+    return result;
 }
