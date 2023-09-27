f575c704c2c2007e21f306504a2340b3b5d0fa2e
Uliana Parkina <uliana.parkina@gmail.com>
2023-09-27 16:39:32 +0300

    style

--- tasks/sort_students/sort_students.cpp
+++ tasks/sort_students/sort_students.cpp
@@ -3,10 +3,10 @@
 #include <algorithm>
 
 bool Date::operator<(const Date& other) const {
-    if (this->year != other.year){
+    if (this->year != other.year) {
         return this->year < other.year;
     }
-    if (this->month != other.month){
+    if (this->month != other.month) {
         return this->month < other.month;
     }
     return this->day < other.day;
@@ -17,27 +17,26 @@ bool Date::operator!=(const Date& other) const {
 }
 
 void SortStudents(std::vector<Student>& students, SortKind sortKind) {
-    if (sortKind == SortKind::Date){
-        sort(students.begin(), students.end(), [](const Student& a, const Student& b){
+    if (sortKind == SortKind::Date) {
+        sort(students.begin(), students.end(), [](const Student& a, const Student& b) {
             if (a.birth_date != b.birth_date) {
                 return a.birth_date < b.birth_date;
             }
-            if (a.last_name != b.last_name){
+            if (a.last_name != b.last_name) {
                 return a.last_name < b.last_name;
             }
             return a.name < b.name;
         });
     }
-    if (sortKind == SortKind::Name){
+    if (sortKind == SortKind::Name) {
         sort(students.begin(), students.end(), [](const Student& a, const Student& b) {
             if (a.last_name != b.last_name) {
                 return a.last_name < b.last_name;
             }
-            if (a.name != b.name){
+            if (a.name != b.name) {
                 return a.name < b.name;
             }
             return a.birth_date < b.birth_date;
         });
     }
 }
-
