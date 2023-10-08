0b004880f8f77748d49cc4a450ab68199c3430a3
Uliana Parkina <uliana.parkina@gmail.com>
2023-10-08 23:04:05 +0300

    tmp

--- tasks/queue/queue.cpp
+++ tasks/queue/queue.cpp
@@ -7,21 +7,36 @@ Queue::Queue() : front_(nullptr), tail_(nullptr), size_(0) {
 }
 
 Queue::~Queue() {
-    Node* cur = front_;
-
+    while (size_ != 0) {
+        Pop();
+    }
 }
 
 void Queue::Push(int32_t value) {
+    Node* new_element = new Node(value, nullptr);
+    if (tail_ == nullptr) {
+        tail_ = front_ = new_element;
+    } else {
+        tail_->next = new_element;
+    }
 }
+
 void Queue::Pop() {
+    Node* nxt = front_->next;
+    delete front_;
+    front_ = nxt;
 }
+
 int32_t Queue::Front() const {
-    return 0;
+    return front_->value;
 }
+
 int32_t Queue::Size() const {
-    return 0;
+    return size_;
 }
+
 void Queue::Clear() {
+
 }
 bool Queue::Empty() const {
     return false;
