5c7d5133dfd4e2af0e9a8886b584aa3e79fe4812
Uliana Parkina <uliana.parkina@gmail.com>
2023-10-08 23:10:46 +0300

    first commit, solve problem

--- tasks/queue/queue.cpp
+++ tasks/queue/queue.cpp
@@ -1,6 +1,6 @@
 #include "queue.h"
 
-Node::Node(int32_t value, Node *next) : value(value), next(next) {
+Node::Node(int32_t value, Node* next) : value(value), next(next) {
 }
 
 Queue::Queue() : front_(nullptr), tail_(nullptr), size_(0) {
@@ -18,13 +18,16 @@ void Queue::Push(int32_t value) {
         tail_ = front_ = new_element;
     } else {
         tail_->next = new_element;
+        tail_ = new_element;
     }
+    ++size_;
 }
 
 void Queue::Pop() {
     Node* nxt = front_->next;
     delete front_;
     front_ = nxt;
+    --size_;
 }
 
 int32_t Queue::Front() const {
@@ -36,8 +39,9 @@ int32_t Queue::Size() const {
 }
 
 void Queue::Clear() {
-
+    this->~Queue();
 }
+
 bool Queue::Empty() const {
-    return false;
+    return size_ == 0;
 }
